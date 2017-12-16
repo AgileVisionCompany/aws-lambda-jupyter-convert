c1d0540d816259cb564704c2856a4ddda7fe195c
Volodymyr Rudyi <volodymyr@agilevision.io>
2017-12-16 13:04:39 +0200

    Initial Import

---
+++ handler.py
@@ -0,0 +1,37 @@
+import json
+import logging
+import nbformat
+import cgi
+from nbconvert.preprocessors import ExecutePreprocessor
+from io import StringIO, BytesIO
+import multipart as mp
+from multipart import tob
+import base64
+
+logger = logging.getLogger(__name__)
+
+def nbformathandler(event, context):
+
+    headers = event['headers'];
+    content_type = headers['content-type']
+    boundary = content_type.split('=')[1]
+    httpbody = mp.MultipartParser(BytesIO(str.encode(event['body'])), boundary)
+    data = httpbody.get('data').file.read()
+
+    in_memory_source = BytesIO(data)
+    nb = nbformat.read(in_memory_source, as_version=4)
+    ep = ExecutePreprocessor(timeout=600, kernel_name='python3')
+    ep.preprocess(nb, {'metadata': {'path': '/tmp/'}})
+
+    ex = StringIO()
+    nbformat.write(nb, ex) 
+
+    res = ex.getValue() 
+    ex.close()      
+
+    response = {
+        "statusCode": 200,
+        "body": res
+    }
+
+    return response
